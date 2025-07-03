4d6abfdd0f5773e251ae07732d3372046a244466
coder-gaia <alexandregaia911@gmail.com>
2025-07-03 20:48:57 -0300

    idk

--- src/api/client.tsx
+++ src/api/client.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import type { InternalAxiosRequestConfig } from 'axios';
 
+console.log('API_URL (VITE):', import.meta.env.VITE_API_URL);
+
 const client = axios.create({
   baseURL: import.meta.env.VITE_API_URL as string,
 });
