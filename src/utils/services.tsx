88643aebec461e1b7503ec3323def3f3c5a4b28e
coder-gaia <alexandregaia911@gmail.com>
2025-07-17 15:07:39 -0300

    changes on services duration

--- src/utils/services.tsx
+++ src/utils/services.tsx
@@ -1,7 +1,7 @@
 export const serviceDurations: Record<string, string> = {
   interior: "50 min",
   exterior: "70 min",
-  inferior: "40 min",
+  inferior: "60 min",
   completa: "75 min",
-  premium:  "85 min",
+  premium:  "90 min",
 };
